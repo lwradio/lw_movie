6d59056849cfef6f50fe5f1649b612f24d0fdc59
lw <1579704169@qq.com>
2023-05-04 11:22:01 +0800

    feat(douban): 豆瓣爬虫工具类重构

--- common/service_base/src/main/java/com/lw/servicebase/config/SwaggerConfig.java
+++ common/service_base/src/main/java/com/lw/servicebase/config/SwaggerConfig.java
@@ -26,11 +26,11 @@ public class SwaggerConfig {
     }
     private ApiInfo webApiInfo(){
         return new ApiInfoBuilder()
-                .title("网站-课程中心API文档")
-                .description("本文档描述了课程中心微服务接口定义")
+                .title("网站-API文档")
+                .description("本文档描述了网站的微服务接口定义")
                 .version("1.0")
                 .contact(new Contact("Helen", "http://atguigu.com",
-                        "55317332@qq.com"))
+                        "lw-wradio@foxmail.com"))
                 .build();
     }
 }
